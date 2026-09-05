9181504ab048d9040ebd28757142196d1b7b0f78
Zenith <168107082+KauaSd@users.noreply.github.com>
2026-09-05 17:33:10 -0300

    Add files via upload

---
+++ Atividade_1.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+st.title("Hi, Im Kauã Silva")
+
+st.header("About Me")
+st.markdown("""
+- working on my **undergraduate thesis**
+- using **React** and **Next.js**
+- Fun fact: I love rhythm games
+### Tech Stacks
+""")
+st.html(
+    """<p align="left">
+  <img src="https://img.shields.io/badge/Python-3776AB?style=for-the-badge&logo=python&logoColor=white" />
+  <img src="https://img.shields.io/badge/FastAPI-005571?style=for-the-badge&logo=fastapi" />
+  <img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black" />
+  <img src="https://img.shields.io/badge/TypeScript-3178C6?style=for-the-badge&logo=typescript&logoColor=white" />
+  <img src="https://img.shields.io/badge/React-20232A?style=for-the-badge&logo=react&logoColor=61DAFB" />
+  <img src="https://img.shields.io/badge/Next.js-000000?style=for-the-badge&logo=nextdotjs&logoColor=white" />
+</p>"""
+)
+st.text("that's it")
